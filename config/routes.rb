4497ebd52761e27cfdd42a17d31b8fa6e642c65c
yuko1117 <eyonseat@yahoo.co.jp>
2020-10-30 11:12:18 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
+  post 'posts/:id', to: 'posts#checked'
 end
